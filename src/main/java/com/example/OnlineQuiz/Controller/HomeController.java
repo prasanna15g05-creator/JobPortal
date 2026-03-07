400409363f73fd39ae0ee690342125462e3cb6ce
Prasanna G <prasannag1505@gmail.com>
2026-03-07 12:40:22 +0530

    Create HomeController.java

---
+++ src/main/java/com/example/OnlineQuiz/Controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.example.OnlineQuiz.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping
+    public String getHome(){
+        return "Backend Running";
+    }
+}
